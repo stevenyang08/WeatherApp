13c2e8c8a25fc49aeb12f2c9858743139baad482
Wellison Pereira <sennad@gmail.com>
2019-03-30 08:54:07 -0400

    WeatherApp: Project Setup commit

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Wellison Pereira on 3/30/19.
+//  Copyright © 2019 Wellison Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
